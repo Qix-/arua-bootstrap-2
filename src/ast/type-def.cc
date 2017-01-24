01e67dfb24e71489622ab100cec95cdd07e62e5d
Josh Junon <junon@uber.com>
2017-01-24 03:41:02 -0800

    allow typedefs to be deferred

--- src/ast/type-def.cc
+++ src/ast/type-def.cc
@@ -3,15 +3,19 @@
 using namespace arua;
 using namespace std;
 
-TypeDef::TypeDef(unsigned int line, unsigned int col, shared_ptr<SymbolRef> baseType)
+TypeDef::TypeDef(unsigned int line, unsigned int col, shared_ptr<SymbolRef> baseType, bool deferred)
 		: Type(line, col, PrimitiveType::DEF)
-		, baseType(baseType) {
+		, baseType(baseType)
+		, deferred(deferred) {
 }
 
 shared_ptr<SymbolRef> TypeDef::getBaseType() const throw() {
 	return this->baseType;
 }
 
+bool TypeDef::isDeferred() const throw() {
+	return this->deferred;
+}
 
 bool TypeDef::equals(const Type &other) const throw() {
 	return this->baseType->resolve()->asType()->equals(other);
