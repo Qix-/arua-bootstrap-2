7ac89a7edd8c2b6c66e2865caf49ca43ae41c05b
Josh Junon <junon@uber.com>
2017-01-24 06:23:49 -0800

    refactor symbol

--- src/ast/symbol.cc
+++ src/ast/symbol.cc
@@ -3,37 +3,10 @@
 using namespace arua;
 using namespace std;
 
-Symbol::Symbol(string identifier, shared_ptr<SymbolContext> symCtx)
-		: identifier(identifier)
-		, symCtx(symCtx) {
+Symbol::Symbol(shared_ptr<SymbolContext> symCtx)
+		: symCtx(symCtx) {
 }
 
-string Symbol::getIdentifier() const throw () {
-	return this->identifier;
-}
-
-shared_ptr<SymbolContext> Symbol::getSymbolContext() const throw() {
+shared_ptr<SymbolContext> Symbol::getContext() const throw() {
 	return this->symCtx;
-}
-
-SymbolType Symbol::getSymbolType() const throw() {
-	return this->symCtx->resolveSymbolEntry(this->identifier)->type;
-}
-
-const shared_ptr<Type> Symbol::asType() const throw() {
-	auto entry = this->symCtx->resolveSymbolEntry(this->identifier);
-	if (entry->type != SymbolType::TYPE) {
-		return nullptr;
-	}
-
-	return static_pointer_cast<SymbolEntry<Type>>(entry)->value;
-}
-
-const shared_ptr<SymbolContext> Symbol::asContext() const throw() {
-	auto entry = this->symCtx->resolveSymbolEntry(this->identifier);
-	if (entry->type != SymbolType::CTXREF) {
-		return nullptr;
-	}
-
-	return static_pointer_cast<SymbolEntry<SymbolContext>>(entry)->value;
 }
--- src/ast/symbol.h
+++ src/ast/symbol.h
@@ -2,34 +2,24 @@
 #define ARUAB_AST_SYMBOL_H__
 #pragma once
 
-#include <string>
 #include <memory>
 
-#include "symbol-context.h"
 #include "token.h"
 
 namespace arua {
 
 class SymbolContext;
 
-class Symbol {
+class Symbol : public Token {
 public:
-	Symbol(std::string identifier, std::shared_ptr<SymbolContext> symCtx);
+	Symbol(std::shared_ptr<SymbolContext> symCtx);
 
-	std::string getIdentifier() const throw();
-	std::shared_ptr<SymbolContext> getSymbolContext() const throw();
+	std::shared_ptr<SymbolContext> getContext() const throw();
 
-	// Note that certain declarations, namely in structs and modules, do not have to be in order (similar to Java)
-	// which means this might return INVALID even for valid symbols that haven't been lexed yet.
-	//
-	// Make sure to only call this after lexing is finished.
-	SymbolType getSymbolType() const throw();
-
-	const std::shared_ptr<Type> asType() const throw();
-	const std::shared_ptr<SymbolContext> asContext() const throw();
+protected:
+	virtual std::shared_ptr<Symbol> resolve() const throw() = 0;
 
 private:
-	std::string identifier;
 	std::shared_ptr<SymbolContext> symCtx;
 };
 
