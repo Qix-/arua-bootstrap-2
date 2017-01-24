4262b1b4aaa390cd106480fd68e821911f479454
Josh Junon <junon@uber.com>
2017-01-24 06:38:19 -0800

    add SymbolIndirect

---
+++ src/ast/symbol-indirect.cc
@@ -0,0 +1,40 @@
+#include "symbol-indirect.h"
+
+using namespace arua;
+using namespace std;
+
+SymbolIndirect::SymbolIndirect(shared_ptr<SymbolContext> symCtx)
+		: Symbol(symCtx) {
+}
+
+unsigned int SymbolIndirect::getLine() const throw() {
+	if (this->identifiers.size() == 0) {
+		return 0;
+	}
+
+	return (*this->identifiers.cbegin())->getLine();
+}
+
+unsigned int SymbolIndirect::getColumnStart() const throw() {
+	if (this->identifiers.size() == 0) {
+		return 0;
+	}
+
+	return (*this->identifiers.cbegin())->getColumnStart();
+}
+
+unsigned int SymbolIndirect::getColumnEnd() const throw() {
+	if (this->identifiers.size() == 0) {
+		return 0;
+	}
+
+	return (*(this->identifiers.cend() - 1))->getColumnEnd();
+}
+
+void SymbolIndirect::addIdentifier(const shared_ptr<Identifier> identifier) throw() {
+	this->identifiers.push_back(identifier);
+}
+
+const vector<const shared_ptr<Identifier>> SymbolIndirect::getIdentifiers() const throw() {
+	return this->identifiers;
+}
---
+++ src/ast/symbol-indirect.h
@@ -0,0 +1,32 @@
+#ifndef ARUAB_AST_SYMBOL_INDIRECT_H__
+#define ARUAB_AST_SYMBOL_INDIRECT_H__
+#pragma once
+
+#include <memory>
+#include <vector>
+
+#include "identifier.h"
+#include "symbol.h"
+#include "target.h"
+
+namespace arua {
+
+class SymbolIndirect : public Symbol, public Target {
+public:
+	SymbolIndirect(std::shared_ptr<SymbolContext> symCtx);
+
+	virtual unsigned int getLine() const throw();
+	virtual unsigned int getColumnStart() const throw();
+	virtual unsigned int getColumnEnd() const throw();
+
+	void addIdentifier(const std::shared_ptr<Identifier> identifier) throw();
+
+	const std::vector<const std::shared_ptr<Identifier>> getIdentifiers() const throw();
+
+private:
+	std::vector<const std::shared_ptr<Identifier>> identifiers;
+};
+
+}
+
+#endif
