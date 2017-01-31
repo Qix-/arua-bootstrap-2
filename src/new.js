ba3c7a9d29786d5af1ff6f228d70d79345103889
Josh Junon <junon@uber.com>
2017-01-30 23:29:11 -0800

    add parens to default definition

--- src/new.js
+++ src/new.js
@@ -43,7 +43,7 @@ function make(name) {
 		'using namespace arua;',
 		'using namespace std;',
 		'',
-		className + '::' + className + ' {',
+		className + '::' + className + '() {',
 		'}',
 		''
 	];
