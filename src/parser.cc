7cdcbdd112f359c72e86406312776ed2bdceb533
Josh Junon <junon@uber.com>
2017-02-01 18:53:19 -0800

    add value parsing (untested)

--- src/parser.cc
+++ src/parser.cc
@@ -1,9 +1,15 @@
+#include <codecvt>
+#include <cstdlib>
 #include <iostream>
 #include <list>
+#include <locale>
 #include <memory>
+#include <sstream>
 #include <stack>
 #include <string>
 
+#include "immediate-number.h"
+#include "immediate-string.h"
 #include "lex-util.h"
 #include "lexer.h"
 #include "parser.h"
@@ -22,6 +28,20 @@ using namespace std;
 
 typedef list<shared_ptr<const Token>> TokenList;
 typedef TokenList::const_iterator TokenListIterator;
+
+enum Parsables {
+	EXPECTED   = 0x01,
+	NUMBER     = 0x02,
+	STRING     = 0x04,
+	TYPE       = 0x08,
+	UNRESOLVED = 0x10,
+	IMMEDIATE  = NUMBER | STRING,
+};
+
+template <typename E>
+bool has(int flags, E v) {
+	return (flags & v) != 0;
+}
 
 template <typename A, typename B>
 struct Tuple {
@@ -93,6 +113,8 @@ struct Tokenizer {
 		this->stack.top() = okItr;
 	}
 };
+
+static wstring_convert<codecvt_utf8<char32_t>, char32_t> utf8Encoder;
 
 ostream & problem(shared_ptr<const Token> t, string severity = "error") {
 	return cerr << "aruab: " << severity << ": " << t->source << ":" << t->line << ":" << t->columnStart << ": ";
@@ -169,7 +191,7 @@ bool parse_pub(Tokenizer &t) {
 	return true;
 }
 
-bool parse_type(Tokenizer &t, Ptr<Type> &type, bool expected = true);
+bool parse_type(Tokenizer &t, Ptr<Type> &type, int allowed = TYPE | UNRESOLVED | EXPECTED);
 
 bool parse_array(Tokenizer &t, Ptr<Type> &type) {
 	if (!expect(t, ABT_OBRACKET)) return unexpected(t);
@@ -293,7 +315,25 @@ bool parse_unresolved_type(Tokenizer &t, Ptr<Type> &type) {
 	return true;
 }
 
-bool parse_type(Tokenizer &t, Ptr<Type> &type, bool expected) {
+bool parse_unresolved_value(Tokenizer &t, Ptr<Value> &value) {
+	if ((*t)->type != ABT_ID) return unexpected(t);
+
+	shared_ptr<TokenList> idents(new TokenList());
+
+	idents->push_back(*t);
+	++t;
+
+	while (expect(t, ABT_DOT)) {
+		if ((*t)->type != ABT_ID) return unexpected(t);
+		idents->push_back(*t);
+		++t;
+	}
+
+	t.unresolvedValues.push_back(Tuple<Ptr<Value>, shared_ptr<TokenList>>(value, idents));
+	return true;
+}
+
+bool parse_concrete_type(Tokenizer &t, Ptr<Type> &type) {
 	// array?
 	if ((*t)->type == ABT_OBRACKET) {
 		return parse_array(t, type);
@@ -314,13 +354,130 @@ bool parse_type(Tokenizer &t, Ptr<Type> &type, bool expected) {
 		return parse_scalar(t, type);
 	}
 
+	return false;
+}
+
+bool parse_immediate_number(Tokenizer &t, Ptr<Value> &value) {
+	switch ((*t)->type) {
+	case ABT_NUM:
+	case ABT_NUM_FLOAT:
+		value = Ptr<ImmediateNumber>::make((*t)->value, 10).as<Value>();
+		return true;
+	case ABT_NUM_RADIX: {
+		int radix = atoi((*t)->value.substr(0, (*t)->value.length() - 1).c_str());
+		if (radix < 0 || radix > 36) {
+			problem(t) << "radix must be between 0 and 36: " << arua::formatToken(*t, true);
+			return false;
+		}
+
+		if (radix == 0) {
+			radix = 16;
+		}
+
+		if ((*++t)->type != ABT_NUM_EX) return unexpected(t);
+
+		value = Ptr<ImmediateNumber>::make((*t)->value, radix).as<Value>();
+		return true;
+	}
+	case ABT_NUM_EX:
+		problem(t, "warning") << "unexpected extended number (no radix); is the lexer feeling OK?: "
+			<< arua::formatToken(*t, true) << endl;
+		// fallthrough
+	default:
+		return unexpected(t);
+	}
+}
+
+static string fromHex(string s) {
+	static_assert('9' < 'A', "ASCII '9' must come before 'A' for this algorithm to work");
+	char32_t result = 0;
+	int shift = 0;
+	int digit = 0;
+	int c;
+	for (ssize_t i = s.length() - 1; i >= 0; i--, shift += 4) {
+		c = toupper(s[i]);
+		digit = c - (c < 'A' ? '0' : ('A' - 10));
+		result |= digit << shift;
+	}
+
+	return utf8Encoder.to_bytes(result);
+}
+
+// also known as string literals
+bool parse_immediate_string(Tokenizer &t, Ptr<Value> &value) {
+	if ((*t)->type != ABT_STR_BEGIN) return unexpected(t);
+
+	stringstream ss;
+
+	while ((*++t)->type != ABT_STR_END) {
+		string code = (*t)->value.substr(2);
+		switch ((*t)->type) {
+		case ABT_STR_LITERAL:
+			ss << (*t)->value;
+			break;
+		case ABT_STR_ESCAPE:
+			switch ((*t)->value[1]) {
+			case 'n': ss << "\n"; break;
+			case 'r': ss << "\r"; break;
+			case 'f': ss << "\f"; break;
+			case 't': ss << "\t"; break;
+			case 'a': ss << "\a"; break;
+			case 'v': ss << "\v"; break;
+			case 'b': ss << "\b"; break;
+			case 'U':
+			case 'u':
+			case 'x': ss << fromHex(code); break;
+			default:
+				problem(t) << "unknown literal: " << arua::formatToken(*t, true) << endl;
+				return false;
+			}
+			break;
+		default:
+			return unexpected(t);
+		}
+	}
+
+	value = Ptr<ImmediateString>::make(ss.str()).as<Value>();
+	return true;
+}
+
+bool parse_type(Tokenizer &t, Ptr<Type> &type, int allowed) {
+	if (has(allowed, TYPE) && parse_concrete_type(t, type)) {
+		return true;
+	}
+
 	// make a deferred resolution type from an identifier if there is one
-	if ((*t)->type == ABT_ID) {
+	if (has(allowed, UNRESOLVED) && (*t)->type == ABT_ID) {
 		return parse_unresolved_type(t, type);
 	}
 
 	// otherwise, this is an invalid token
-	return expected ? unexpected(t) : false;
+	return has(allowed, EXPECTED) ? unexpected(t) : false;
+}
+
+bool parse_value(Tokenizer &t, Ptr<Value> &value, int allowed) {
+	Ptr<Type> type;
+	if (has(allowed, TYPE) && parse_concrete_type(t, type)) {
+		value = type.as<Value>();
+		return true;
+	}
+
+	if (has(allowed, NUMBER)
+			&& ((*t)->type == ABT_NUM
+			|| (*t)->type == ABT_NUM_RADIX
+			|| (*t)->type == ABT_NUM_FLOAT)) {
+		return parse_immediate_number(t, value);
+	}
+
+	if (has(allowed, STRING) && (*t)->type == ABT_STR_BEGIN) {
+		return parse_immediate_string(t, value);
+	}
+
+	if (has(allowed, UNRESOLVED) && (*t)->type == ABT_ID) {
+		return parse_unresolved_value(t, value);
+	}
+
+	return false;
 }
 
 bool parse_typedef(Tokenizer &t, Ptr<Module> module) {
@@ -395,7 +552,7 @@ bool resolve(Tokenizer &t, Ptr<Context> ctx) {
 				}
 
 				Ptr<Value> tmpValue = curValue;
-				curValue = curValue.as<Context>()->resolve(tkn->value, prevValue.as<Context>());
+				curValue = curValue.as<Context>()->resolve(tkn->value, prevValue);
 				prevValue = tmpValue;
 				lastToken = tkn;
 
